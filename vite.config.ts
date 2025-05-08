74defea651c763ee793e0be5b52bbf19329f0aa3
ArthurGomesRosa <arthurrosaofc@gmail.com>
2025-05-08 10:34:21 -0300

    update 4

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/AGR-Portfolio/',
+  base: '/ArthurRosa-Portfolio/',
   plugins: [react()],
 })
